0f41e4ff8e758b8b3c8c533e90cdeff726ca60bc
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-02-10 20:23:46 +0300

    Добавляет пропсы с массивом фильмов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {filmName, filmGenre, filmReleaseDate} = props;
+const App = ({filmName, filmGenre, filmReleaseDate, allFilms}) => {
 
   return (
     <Main
       filmName={filmName}
       filmGenre={filmGenre}
       filmReleaseDate={filmReleaseDate}
+      allFilms={allFilms}
     />
   );
+};
+
+App.propTypes = {
+  filmName: PropTypes.string.isRequired,
+  filmGenre: PropTypes.string.isRequired,
+  filmReleaseDate: PropTypes.number.isRequired,
+  allFilms: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
