19ba64841ed89697c3f9eb2ca8cfd06d641ddd8e
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-02-16 15:36:26 +0300

    Обновляет старые тесты и добавляет новые

--- src/components/movies-list/movies-list.test.js
+++ src/components/movies-list/movies-list.test.js
@@ -5,35 +5,91 @@ import MoviesList from "./movies-list.jsx";
 const films = [
   {
     title: `The No Flowers`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `Rings of Cloud`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `The Sword's Memory`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `The Voyagers of the Words`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `Shores in the Heart`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `Missing Illusion`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `The Unwilling Man`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   },
   {
     title: `Teacher in the Hunter`,
-    image: `http://placehold.it/280x175`
+    image: `http://placehold.it/280x175`,
+    description: `qwerty`,
+    rating: 1.0,
+    numberOfVotes: 1982,
+    director: `Taika Di Caprio`,
+    actors: `Ashley Cooper`,
+    genre: `Dramas`,
+    year: 1900
   }
 ];
 
@@ -41,6 +97,7 @@ it(`Render MoviesList`, () => {
   const tree = renderer
     .create(<MoviesList
       films={films}
+      onCardClick={() => {}}
     />)
     .toJSON();
 
